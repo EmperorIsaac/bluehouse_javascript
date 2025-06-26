8ed2f9d7cff1a31c6b1b851219cebee9b604242f
EmperorIsaac <isaactitusive@gmail.com>
2025-06-26 06:34:10 -0700

    update

---
+++ javascript/try.js
@@ -0,0 +1,26 @@
+var user = {
+  username: "john_doe",
+  password: "supersecure123"
+};
+
+var database = [
+	 {
+  username: "john_doe",
+  password: "supersecure123"
+}
+];
+
+var newsfeed = [
+  {
+    username: "Stanley",
+    timeline: "Feeling like Stan!"
+  },
+  {
+    username: "Mariom",
+    timeline: "Had a great workout today!"
+  },
+  {
+    username: "Remson",
+    timeline: "He is a hustler!"
+  }
+];
